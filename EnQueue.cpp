8fdc63108a013aaaa70b9e43e50cfd50b721516a
hiiamtruongnhudat <dattn.b19at039@stu.ptit.edu.vn>
2021-06-14 21:11:21 +0700

    bfs using EnQueue

--- EnQueue.cpp
+++ EnQueue.cpp
@@ -1,7 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
+vector<int> adj[1001];
+bool visited[1001];
+int V, E, S;
 class Node
 {
 public:
@@ -83,14 +85,45 @@ void Queue::Display()
         deQueue();
     }
 }
-
-int main()
+void BFS(int node)
 {
     Queue q;
-    for (int i = 1; i <= 10; i++)
-        q.enQueue(i);
+    q.enQueue(node);
+    while (!q.isEmpty())
+    {
+        int u = q.front(); q.deQueue();
+        cout << u << " ";
+        visited[u] = true;
+        for (int i = 0 ; i < adj[u].size(); ++i)
+            if(!visited[adj[u][i]])
+            {
+                visited[adj[u][i]] = true;
+                q.enQueue(adj[u][i]);
+            }
+    }
+}
+void test_case()
+{
+    memset(visited,false,sizeof(visited));
+    for (int i = 0; i <= 1001; ++i)
+        adj[i].clear();
+    cin >> V >> E >> S;
+    for (int i = 0 ; i < E ;++i)
+        {
+            int v,w;
+            cin >> v >> w;
+            adj[v].push_back(w);
+            // adj[w].push_back(v);
+        }
+    BFS(S);
     cout << "\n";
-    int  x = q.front();
-    cout << x;
+}
+int main()
+{
+    int t; cin >> t;
+    while(t--)
+    {
+        test_case();
+    }
     return 0;
 }
